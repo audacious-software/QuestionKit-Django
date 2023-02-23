33f3fc57f637f5e1079f60d70cb5bd34b034d9e2
Chris J. Karr <chris@audacious-software.com>
2023-02-23 22:20:54 +0000

    Codebook work

---
+++ pdk_api.py
@@ -0,0 +1,4 @@
+
+def update_data_type_definition(definition, override_existing=False): # pylint:disable=unused-argument
+    pass
+    # print('QUESTION KIT: Update definition: %s' % json.dumps(definition, indent=2))
